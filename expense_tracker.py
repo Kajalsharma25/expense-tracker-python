9ea6b395e79154234e696a92d7e9fc7e2857f27a
Kajal sharma <108068644+Kajalsharma25@users.noreply.github.com>
2026-04-10 18:24:48 +0530

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,1 +1,10 @@
+print("welcome to expense tracker")
+expenses = [200,300,500,700,1000]
+total = Sum(expenses)
+highest = max(expenses)
+average = Sum(expenses) / len(expenses)
+print("Expenses:", expenses)
+print("Total:", total)
+print("Highest:", highest)
+print("Average:", average)
 
